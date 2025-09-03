61e07c922d8f569363d3ceca3518ca22fd9a9647
Emmanuel Mendez <emmanuelmendez.dev@outlook.com>
2025-09-03 19:08:02 -0400

    feat: update dependencies and add cloadflare middleware support
    - Added @astrojs/cloudflare and wrangler as dependencies and devDependencies in package.json.
    - Introduced middleware support with the creation of middleware-insertion-facade.js and middleware-loader.entry.ts.
    - Implemented no-op worker and error handling middleware for improved request processing.
    - Configured assets ignore for worker files and added wrangler configuration for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  adapter: cloudflare()
 });
